37c9cb769fd38b4d9d3f13e1baeb3d31e65bb995
ForsanNemah <forsan20172017@gmail.com>
2022-12-14 16:55:26 +0300

    edit auth route

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('Auth.login');
+    return view('auth.login');
 });
 
 Route::get('/register', function () {
-    return view('Auth.login');
+    return view('auth.login');
 });
 
 
